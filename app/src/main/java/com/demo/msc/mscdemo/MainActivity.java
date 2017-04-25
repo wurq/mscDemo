35c97f9ddd3be6adaaac17837b0c4db01fbc73b9
wurongqiu <wurongqiu@gmail.com>
2017-04-25 13:23:17 +0800

    2

--- app/src/main/java/com/demo/msc/mscdemo/MainActivity.java
+++ app/src/main/java/com/demo/msc/mscdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.demo.msc.mscdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // 1
+//2
 public class MainActivity extends AppCompatActivity {
 
     @Override
