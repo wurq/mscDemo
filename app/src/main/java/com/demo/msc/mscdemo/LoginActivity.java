e072ab2313d2349e65c3a859f5154a58ee6136c5
wurongqiu <wurongqiu@gmail.com>
2017-05-02 11:21:52 +0800

    2 change launch intent

--- app/src/main/java/com/demo/msc/mscdemo/LoginActivity.java
+++ app/src/main/java/com/demo/msc/mscdemo/LoginActivity.java
@@ -3,15 +3,11 @@ package com.demo.msc.mscdemo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * 用户名输入页面
@@ -37,23 +33,24 @@ public class LoginActivity extends Activity implements OnClickListener{
 		case R.id.btn_confirm:
 			// 过滤掉不合法的用户名
 			String uname = ((EditText) findViewById(R.id.edt_uname)).getText().toString();
-			if (TextUtils.isEmpty(uname)) {
-				showTip("用户名不能为空");
-				return;
-			} else {
-				Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
-				Matcher m = p.matcher(uname);
-				if (m.find()) {
-					showTip("不支持中文字符");
-					return;
-				} else if (uname.contains(" ")) {
-					showTip("不能包含空格");
-					return;
-				} else if (!uname.matches("^[a-zA-Z][a-zA-Z0-9_]{5,17}")) {
-					showTip("6-18个字母、数字或下划线的组合，以字母开头");
-					return;
-				} 
-			}
+//			String uname = ((EditText) findViewById(R.id.edt_uname)).getText().toString();
+//			if (TextUtils.isEmpty(uname)) {
+//				showTip("用户名不能为空");
+//				return;
+//			} else {
+//				Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+//				Matcher m = p.matcher(uname);
+//				if (m.find()) {
+//					showTip("不支持中文字符");
+//					return;
+//				} else if (uname.contains(" ")) {
+//					showTip("不能包含空格");
+//					return;
+//				} else if (!uname.matches("^[a-zA-Z][a-zA-Z0-9_]{5,17}")) {
+//					showTip("6-18个字母、数字或下划线的组合，以字母开头");
+//					return;
+//				}
+//			}
 			
 			Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 			intent.putExtra("uname", uname);
